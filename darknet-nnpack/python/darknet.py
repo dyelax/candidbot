f663fd7dd1d86ed0b22173b7f7679d81900bd5b1
Matt Cooper <matthew_cooper@brown.edu>
2018-11-07 11:33:33 -0500

    updated darknet python

--- darknet-nnpack/python/darknet.py
+++ darknet-nnpack/python/darknet.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
+    net = load_net("cfg/yolov3-tiny.cfg", "yolov3-tiny.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
     print r
