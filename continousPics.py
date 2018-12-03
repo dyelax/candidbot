d7bd0a0e578eeeec97de31db24bc84731d2ecc57
Matt Cooper <matthew_cooper@brown.edu>
2018-12-03 16:15:10 -0500

    cleanup

--- continousPics.py
+++ continousPics.py
@@ -1,12 +1,12 @@
-
 from time import sleep
 from picamera import PiCamera
 import os
-os.chdir ("/home/pi/candidbot/images")
+
+os.chdir("/home/pi/candidbot/images")
 
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
 for filename in camera.capture_continuous('{timestamp}.jpg'):
-        print('Captured %s' % filename)
-        sleep(1) # wait 5 minutes
+  print('Captured %s' % filename)
+  sleep(1)
--- tracking_test.py
+++ tracking_test.py
@@ -56,7 +56,7 @@ def main():
   tracker = Tracker(160, 30, 5, 100)
 
   vid_writer = cv2.VideoWriter(
-    'test/tracking-processed.mp4',
+    'test/tracking-processed.avi',
     cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
     30,
     (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
