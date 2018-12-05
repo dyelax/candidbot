3b65d8a4521a691824ff3cad51e61b5e829ebb83
Ben Gross <benjaminperelsongross@gmail.com>
2018-12-05 11:16:49 -0500

    wheel controls

--- motion_controller.py
+++ motion_controller.py
@@ -1,25 +1,66 @@
+import RPi.GPIO as gpio
+import time
 
 
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
 
 def turn_left():
   # TODO: Make the wheels turn the bot a little bit left
-  pass
+    init()
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(1)
+    gpio.cleanup()
+
 
 def turn_right():
   # TODO: Make the wheels turn the bot a little bit right
-  pass
+    init()
+    gpio.output(17, False)
+    gpio.output(22, True)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(1)
+    gpio.cleanup()
 
 def turn_90():
   # TODO: Turn left 90 degrees
-  pass
+    init()
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(4)
+    gpio.cleanup()
 
 def go_forward():
   # TODO: Make the wheels move the bot a little bit forward
-  pass
+    init()
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(1)
+    gpio.cleanup()
+
 
 def go_backward():
-  # TODO: Make the wheels move the bot a little bit backward
-  pass
+    # TODO: Make the wheels move the bot a little bit backward
+    init()
+    gpio.output(17, False)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(1)
+    gpio.cleanup()
+
 
 PROXIMITY_THRESH = 100  # TODO: play with this number
 def proximity_warning():
