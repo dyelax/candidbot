0ed67812574759db49629b2d11bf11af96cdec7a
Ben Gross <benjaminperelsongross@gmail.com>
2018-12-11 18:37:36 -0500

    manual test

--- manual_drive.py
+++ manual_drive.py
@@ -4,15 +4,19 @@ from motion_controller import MotionController
 motion_controller = MotionController()
 
 while 1:
-    key = input()
-    if key == 'f':
-        motion_controller.go_forward()
-    elif key == 'r':
-        motion_controller.turn_right()
-    elif key == 'l':
-        motion_controller.turn_left()
-    elif key == 'v':
-        motion_controller.turn_90()
-    elif key == 'b':
-        motion_controller.go_backward()
+    try:
+        key = input('enter command')
+        if key == 'f':
+            motion_controller.go_forward()
+        elif key == 'r':
+            motion_controller.turn_right()
+        elif key == 'l':
+            motion_controller.turn_left()
+        elif key == 'v':
+            motion_controller.turn_90()
+        elif key == 'b':
+            motion_controller.go_backward()
+    except KeyboardInterrupt:
+        motion_controller.close()
+        exit(0)
 
