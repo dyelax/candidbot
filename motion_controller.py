fdea0ea68a157cdb550cc52469f78873e65cef7e
Matt Cooper <matthew_cooper@brown.edu>
2018-12-11 21:05:14 -0500

    back to continuous movement

--- motion_controller.py
+++ motion_controller.py
@@ -4,9 +4,8 @@ import time
 
 class MotionController:
   def __init__(self):
-    pass
-    # self.init_wheels()
-    # self.stop()
+    self.init_wheels()
+    self.stop()
 
   def init_wheels(self):
     gpio.setmode(gpio.BCM)
@@ -16,65 +15,52 @@ class MotionController:
     gpio.setup(24, gpio.OUT)
 
   def turn_left(self):
-    self.init_wheels()
     # Make the wheels turn the bot a little bit left
-    # self.stop()
+    self.stop()
 
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, True)
     time.sleep(0.2)
-    # time.sleep(4)
 
-    # self.stop()
-    gpio.cleanup()
-
+    self.stop()
+    time.sleep(1)
 
   def turn_right(self):
-    self.init_wheels()
     # Make the wheels turn the bot a little bit right
-    # self.stop()
+    self.stop()
 
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, True)
     gpio.output(24, False)
     time.sleep(0.2)
-    # time.sleep(4)
 
-    # self.stop()
-    gpio.cleanup()
-
+    self.stop()
+    time.sleep(1)
 
   def turn_90(self):
-    self.init_wheels()
     # Turn left 90 degrees
-    # self.stop()
+    self.stop()
 
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, True)
-    # time.sleep(2)
     time.sleep(1)
 
-    # self.stop()
-    gpio.cleanup()
-
+    self.stop()
+    time.sleep(1)
 
   def stop(self):
-    self.init_wheels()
     gpio.output(17, False)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, False)
     time.sleep(4)
-    gpio.cleanup()
-
 
   def go_forward(self):
-    self.init_wheels()
     # Make the wheels move the bot a little bit forward
     gpio.output(17, True)
     gpio.output(22, False)
@@ -82,11 +68,7 @@ class MotionController:
     gpio.output(24, False)
     time.sleep(1)
 
-    gpio.cleanup()
-
-
   def go_backward(self):
-    self.init_wheels()
     # Make the wheels move the bot a little bit backward
     gpio.output(17, False)
     gpio.output(22, True)
@@ -101,17 +83,3 @@ class MotionController:
   def close(self):
     self.stop()
     gpio.cleanup()
-
-
-  def proximity_warning_center(self):
-    # TODO: Return whether the distance from the center proximity sensor is closer than PROXIMITY_THRESH
-    pass
-
-  def proximity_warning_left(self):
-    # TODO: Return whether the distance from the left proximity sensor is closer than PROXIMITY_THRESH
-    pass
-
-  def proximity_warning_right(self):
-    # TODO: Return whether the distance from the right proximity sensor is closer than PROXIMITY_THRESH
-    pass
-
