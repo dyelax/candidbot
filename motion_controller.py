cc920d067f42c25b863d04f7183e851612f927d3
Matt Cooper <matthew_cooper@brown.edu>
2018-12-11 19:25:11 -0500

    init/cleanup after each command

--- motion_controller.py
+++ motion_controller.py
@@ -4,17 +4,20 @@ import time
 
 class MotionController:
   def __init__(self):
+    self.init_wheels()
+    self.stop()
+
+  def init_wheels(self):
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
 
-    self.stop()
-
   def turn_left(self):
+    self.init_wheels()
     # Make the wheels turn the bot a little bit left
-    # self.stop()
+    self.stop()
 
     gpio.output(17, True)
     gpio.output(22, False)
@@ -23,9 +26,12 @@ class MotionController:
     # time.sleep(0.2)
     time.sleep(2)
 
-    # self.stop()
+    self.stop()
+    gpio.cleanup()
+
 
   def turn_right(self):
+    self.init_wheels()
     # Make the wheels turn the bot a little bit right
     self.stop()
 
@@ -37,8 +43,11 @@ class MotionController:
     time.sleep(2)
 
     self.stop()
+    gpio.cleanup()
+
 
   def turn_90(self):
+    self.init_wheels()
     # Turn left 90 degrees
     self.stop()
 
@@ -50,27 +59,38 @@ class MotionController:
     time.sleep(10)
 
     self.stop()
+    gpio.cleanup()
+
 
   def stop(self):
+    self.init_wheels()
     gpio.output(17, False)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, False)
     time.sleep(0.2)
+    gpio.cleanup()
+
 
   def go_forward(self):
+    self.init_wheels()
     # Make the wheels move the bot a little bit forward
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, True)
     gpio.output(24, False)
+    gpio.cleanup()
+
 
   def go_backward(self):
+    self.init_wheels()
     # Make the wheels move the bot a little bit backward
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, False)
     gpio.output(24, True)
+    gpio.cleanup()
+
 
   def close(self):
     self.stop()
